23e1677537b607598737c9f251d221780f395d7a
Scott McClure <mccluresc@gmail.com>
2016-07-01 15:20:57 -0400

    Initial revision of game

---
+++ script.js
@@ -0,0 +1,89 @@
+var guesses = [];
+var misses = 0;
+var word = "ABALONE";
+
+function updateWord() {
+  $("#word").empty();
+  for (var i = 0; i < word.length; i++) {
+    var letter = word.charAt(i);
+    if (guesses.includes(letter)) {
+      $("#word").append(letter);
+    } else {
+      $("#word").append("_");
+    }
+  }
+}
+
+function updateHangman() {
+  $("#hangman").attr("src", "Hangman-" + misses + ".png");
+}
+
+function hasWon() {
+  for(var i = 0; i < word.length; i++) {
+    var letter = word.charAt(i);
+    if (guesses.includes(letter) == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function hasLost() {
+  if (misses < 6) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function guessLetter(letter) {
+  letter = letter.toUpperCase();
+
+  if (guesses.includes(letter)) {
+    $("#message").text("You already guessed the letter '" + letter + "'!");
+    $("#message").show();
+    return;
+  }
+
+  guesses.push(letter);
+
+  if (word.includes(letter) == false) {
+    misses++;
+  }
+}
+
+function newGame() {
+  guesses = [];
+  misses = 0;
+  $("#message").hide();
+  $("#newgame").hide();
+  updateWord();
+  updateHangman();
+}
+
+$(document).ready(function() {
+  newGame();
+
+  $("#newgame").click(newGame);
+
+  $(document).keypress(function(event) {
+    $("#message").empty();
+    $("#message").hide();
+
+    guessLetter(event.key);
+
+    updateWord();
+    updateHangman();
+
+    if (hasWon()) {
+      $("#message").text("You won!");
+      $("#message").show();
+      $("#newgame").show();
+    }
+    if (hasLost()) {
+      $("#message").text("You lost!");
+      $("#message").show();
+      $("#newgame").show();
+    }
+  });
+});
